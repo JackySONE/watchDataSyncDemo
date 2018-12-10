09bae89eba6b821be71677f3713970df17b38c15
JackySONE <jackysnsd@gmail.com>
2018-12-10 14:15:27 +0800

    [Init] Init Commit

---
+++ WatchDataSyncDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchDataSyncDemo
+//
+//  Created by JackySONE on 2018/12/10.
+//  Copyright © 2018 JackySONE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
