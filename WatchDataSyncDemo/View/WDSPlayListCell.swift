ca1909c61544b0802041a6da553ee4db64935f0f
JackySONE <jackysnsd@gmail.com>
2018-12-10 14:52:25 +0800

    [Add] 建立播放清單編輯頁UI

---
+++ WatchDataSyncDemo/View/WDSPlayListCell.swift
@@ -0,0 +1,17 @@
+//
+//  WDSPlayListCell.swift
+//  WatchDataSyncDemo
+//
+//  Created by JackySONE on 2018/12/10.
+//  Copyright © 2018 JackySONE. All rights reserved.
+//
+
+import UIKit
+
+class WDSPlayListCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var favoriteScoreLabel: UILabel!
+    @IBOutlet weak var describeLabel: UILabel!
+
+}
--- WatchDataSyncDemo/ViewController.swift
+++ WatchDataSyncDemo/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func onAddTapped() {
 
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "WDSPlayListCell") as? WDSPlayListCell else { return UITableViewCell() }
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 63
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+    }
 }
 
